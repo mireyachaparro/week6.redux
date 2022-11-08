102bb764d625ff002071ee162d626df02d9378af
mireyachaparro <mireyacn99@gmail.com>
2022-11-08 18:39:49 +0100

    Supposed to be ok to netlify

---
+++ build/static/js/874.6cd8cc14.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkweek4_todo=self.webpackChunkweek4_todo||[]).push([[874],{874:function(e,n,t){t.r(n);var u=t(184);n.default=function(){return(0,u.jsx)("main",{children:(0,u.jsx)("h2",{children:"Home page"})})}}}]);
+//# sourceMappingURL=874.6cd8cc14.chunk.js.map
